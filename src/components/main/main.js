1152dd9c3856ae3febe065df6c7fbb2253c15f73
yael <64949819+yael118@users.noreply.github.com>
2024-12-04 16:36:48 +0200

    Update main.js
    add judge to list

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -14,21 +14,18 @@ const data = {
         audioState: false,
         isJudge: false,
         imgUrl:"https://cdn.pixabay.com/photo/2018/11/08/23/52/man-3803551_1280.jpg"
-        //require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_2253.jpg')
     },
+  // {
+  //      position: 'משיב',
+  //      userId: 741,
+  //      description: '',
+  //      fullName: '  רן חיימוביץ',
+  //      videoState: false,
+  //      audioState: false,
+  //      isJudge: false,
+  //      imgUrl: "https://cdn.pixabay.com/photo/2015/01/08/18/30/entrepreneur-593371_1280.jpg"
+  //  },
     {
-        position: 'משיב',
-        userId: 741,
-        description: '',
-        fullName: '  רן חיימוביץ',
-        videoState: false,
-        audioState: false,
-        isJudge: false,
-        imgUrl: "https://cdn.pixabay.com/photo/2015/01/08/18/30/entrepreneur-593371_1280.jpg"
-//        "require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_2253.jpg')
-    }
-    ,
-    ,{
         position: 'עורר',
         userId: 123,
         description: 'עורך דין',
@@ -37,17 +34,16 @@ const data = {
         audioState: false,
         isJudge: false,
         imgUrl: "https://cdn.pixabay.com/photo/2015/01/08/18/30/man-593372_1280.jpg"
-        //require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_4116.jpg')
-    },{
+    },
+    {
         position: 'עורר',
         userId: 321,
         description: 'עורך דין',
-        fullName: 'נעמי קליין',
+        fullName: 'דני כץ',
         videoState: true,
         audioState: true,
         isJudge: false,
-        imgUrl: "https://cdn.pixabay.com/photo/2015/06/22/08/38/children-817368_1280.jpg"
-       // require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_3410.jpg')
+        imgUrl: "https://cdn.pixabay.com/photo/2021/07/14/17/32/manager-6466713_1280.jpg"
     },
     {
         position: 'שופט',
@@ -57,20 +53,18 @@ const data = {
         videoState: true,
         audioState: true,
         isJudge: true,
-        imgUrl: "https://cdn.pixabay.com/photo/2024/01/29/20/40/cat-8540772_1280.jpg"
-        //"require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_3410.jpg')
-    }
-    /*,{
+        imgUrl: "https://cdn.pixabay.com/photo/2015/06/08/18/37/hammer-802298_1280.jpg"
+    },
+    {
         position: 'שופט',
         userId: 987,
-        description: 'כבוד השופטת',
-        fullName: 'יעל קליין',
+        description: 'כבוד השופט',
+        fullName: 'רם זנבר',
         videoState: true,
         audioState: false,
         isJudge: true,
         imgUrl: "https://cdn.pixabay.com/photo/2015/03/11/09/35/african-668398_1280.jpg"
-        //require('C:\\Users\\yael\\Documents\\digital-prog\\src\\images\\IMG_3571.jpg')
-    }*/
+    }
 
 ],
 currUser : {
